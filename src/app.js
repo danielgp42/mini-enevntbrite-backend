184f7b18329ea8056e8afeb656183a40ad3ce089
danielgp <danielgarduno282@gmail.com>
2025-08-31 18:42:17 -0600

    Feat: Sea agregan endpoints para la parte de tickets y se corrigen errrores de sintaxis

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { errorHandler, notFoundHandler } from './middlewares/error.js';
 
 export function buildApp() {
     const app = express();
+    app.use(helmet());
     app.use(cors({origin: env.corsOrigin, credentials: true}));
     app.use(express.json({limit: '2mb'}));
     app.use(cookieParser());
